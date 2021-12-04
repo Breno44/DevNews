c67734fb65c2688d209bc02a3cd2442788e9a02e
Breno <bhfs.2908@gmail.com>
2021-12-04 16:01:25 -0300

    feat:add posts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,46 @@
-import styles from "../styles/Home.module.scss";
+import { GetServerSideProps } from "next";
+import { useEffect, useState } from "react";
 
-export default function Home() {
+interface Post {
+  id: string;
+  title: string;
+}
+
+interface HomeProps {
+  posts: Post[];
+}
+
+export default function Home({ posts }: HomeProps) {
+  // const [posts, setPosts] = useState<Post[]>([]);
+
+  // useEffect(() => {
+  //   fetch("http://localhost:3333/posts").then((response) => {
+  //     response.json().then((data) => {
+  //       setPosts(data);
+  //     });
+  //   });
+  // }, []);
+
   return (
-    <h1>
-      Hello <span>World</span>
-    </h1>
+    <div>
+      <h1>Home</h1>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+    </div>
   );
 }
+
+export const getServerSideProps: GetServerSideProps<HomeProps> = async () => {
+  const response = await fetch("http://localhost:3333/posts");
+
+  const posts = await response.json();
+
+  return {
+    props: {
+      posts,
+    },
+  };
+};
